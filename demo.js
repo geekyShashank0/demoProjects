0cead3e72d716520d05e0ce65b1d85de70211dd6
geekyShashank0 <sss4sep@gmail.com>
2022-03-24 14:31:40 +0530

    Last commit

--- demo.js
+++ demo.js
@@ -1,1 +1,10 @@
 console.log("Hello World") ;
+
+const a = 1 ;
+if(a == 1){
+   console.log("a is 1") ;
+} else {
+   console.log("a is not 1, but the value of a is " + a) ;
+}
+
+console.log("This is end of program") ;
